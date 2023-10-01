04ea6940623c1d607c0c1e5b2760fd05ef87c4eb
shani_levi <shanilevi01011@gmail.com>
2023-10-01 13:46:27 +0300

    sidebar styling

--- components/shard/LeftSidebar.tsx
+++ components/shard/LeftSidebar.tsx
@@ -1,17 +1,38 @@
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
+import { usePathname, useRouter } from 'next/navigation'
 
 import { sidebarLinks } from '@/constants'
 export default function LeftSidebar() {
+  const router = useRouter()
+  const pathname = usePathname()
+
   return (
     <section className='custom-scrollbar leftsidebar'>
       <div className='flex w-full flex-1 flex-col gap-6 px-6'>
-        {sidebarLinks.map((link) => (
-          <Link href={link.route} key={link.label} className='leftsidebar_link'>
-            <Image src={link.imgURL} alt={link.label} width={24} height={24} />
-            <p className='text-light-1 max-lg:hidden'>{link.label}</p>
-          </Link>
-        ))}
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`leftsidebar_link
+            ${isActive && 'bg-primary-500'}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className='text-light-1 max-lg:hidden'>{link.label}</p>
+            </Link>
+          )
+        })}
       </div>
     </section>
   )
